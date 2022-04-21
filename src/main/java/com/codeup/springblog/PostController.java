b743c6fd3218ff5e83baa9fbca8170d3b76ced49
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-21 11:16:50 -0500

    added the html

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -6,17 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    public String showPosts() {
-         return "posts/index";
+    public String showPosts(Model model) {
+
+        ArrayList<Post> allPosts = new ArrayList<>();
+        allPosts.add(new Post("doggos", "i have some puppies"));
+        allPosts.add(new Post("Lost my car", "I lost my car and can't find it help"));
+        allPosts.add(new Post("I'm not from this planet...", "I need help getting back to my planet"));
+        model.addAttribute("posts", allPosts);
+        return "posts/index";
     }
 
     @GetMapping("posts/{id}")
     public String showPostsId(@PathVariable long id, Model model){
-        model.addAttribute( "singlePosts" ,new Post("title", "description"))
-        return "posts/index";
+        model.addAttribute( "singlePosts" ,new Post("title", "description"));
+        return "posts/show";
     }
 
     @GetMapping("posts/create")
